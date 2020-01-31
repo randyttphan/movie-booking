75407a071490cd6cd15abcc4d16ac4271664530c
randyttphan <randyttphan@gmail.com>
2020-01-31 12:48:17 -0600

    populating ui with local storage data.

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 
 const movieSelect = document.getElementById("movie");
+
+populateUI();
+
 let ticketPrice = +movieSelect.value;
 
 //save selected movie index and price
@@ -28,6 +31,25 @@ function updateSelectedCount() {
   total.innerText = countedSeats * ticketPrice;
 }
 
+//function to populate the user interface by grabbing data from local storage
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 //movie select event
 movieSelect.addEventListener("change", e => {
   ticketPrice = +e.target.value;
@@ -46,3 +68,7 @@ container.addEventListener("click", e => {
     updateSelectedCount();
   }
 });
+
+//initial count and set total
+
+updateSelectedCount();
